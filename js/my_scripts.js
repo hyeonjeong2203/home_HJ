1598275659a61db0ccba56875cdc40f524fdf664
hyeonjeong2203 <hyeonjeong2203@gmail.com>
2023-12-21 03:59:34 +0900

    landing page commit 2

--- js/my_scripts.js
+++ js/my_scripts.js
@@ -32,3 +32,39 @@ document.addEventListener("DOMContentLoaded", function () {
 
         type(); // 초기 호출
     });
+
+
+
+
+        // loadAssignment 함수 정의
+function loadAssignment(url) {
+    // iframe에 해당 HTML 파일 로드
+    var iframe = document.getElementById("iframe-container");
+    iframe.src = url;
+
+    // 컨테이너에 'closed' 클래스 제거 (초기에는 추가하지 않음)
+    iframe.classList.remove("closed");
+
+    var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+    var viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+
+    var iframeWidth = viewportWidth * 0.8;
+    var iframeHeight = viewportHeight * 0.6;
+
+    iframe.style.width = iframeWidth + "px";
+    iframe.style.height = iframeHeight + "px";
+    iframe.style.marginTop = "10px";
+}
+
+// closeAssignment 함수 정의
+function closeAssignment() {
+    // iframe에 'closed' 클래스 추가
+    var iframe = document.getElementById("iframe-container");
+    iframe.classList.add("closed");
+
+    // iframe 크기를 0으로 설정하여 숨김
+    iframe.style.width = "0";
+    iframe.style.height = "0";
+    iframe.style.marginTop = "innitial";
+
+}
